ff95c3eb694fc020008efda39817546d80e70660
Van-T-Nguyen <46911024+Van-T-Nguyen@users.noreply.github.com>
2019-10-13 14:16:36 -0500

    Add files via upload

---
+++ HelloWorld_fn.c
@@ -0,0 +1,22 @@
+/* Author  : Van Nguyen
+ * Class   : Data Structures
+ * Project : Hello, World
+ * Due Date: 2019.01.25
+*/
+
+#include <stdio.h>
+
+void hello_world(void);
+
+int main(void)
+{
+	hello_world();
+	getchar();
+	getchar();
+	return 0;
+}
+
+void hello_world()
+{
+	printf("Hello, World!\n");
+}
